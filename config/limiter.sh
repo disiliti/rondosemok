98dfe3dfe6a19234f23c74633cc32a22eccf47f7
disiliti <silitaang@gmail.com>
2026-01-07 05:39:14 +0700

    Update limiter.sh

--- config/limiter.sh
+++ config/limiter.sh
@@ -6,7 +6,7 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 install_services() {
-    local REPO="https://raw.githubusercontent.com/vermilii/Xray-Only/main/files/"
+    local REPO="https://raw.githubusercontent.com/disiliti/rondosemok/main/files/"
     local LIMIT_IP_SERVICES=("limiter-vm" "limiter-vl" "limiter-trj" "limiter-shd")
     local LIMIT_IP_REMOTE_FILES=("lite-vm" "lite-vl" "lite-trj" "lite-shd")
     local LIMIT_QUOTA_SERVICES=("limitvmess" "limitvless" "limittrojan" "limitshadowsocks")
